86f97b3f95706d87a0658ded338a3ec66f13b420
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-20 18:27:33 +0100

    Rozpracováno 3 - Matice.

--- Zaklady CSharp/CSharp_2_Posloupnosti/Program.cs
+++ Zaklady CSharp/CSharp_2_Posloupnosti/Program.cs
@@ -15,7 +15,7 @@ namespace CSharp_2_Posloupnosti
             int[] z = new int[] { 1, 2, 3, 4, 5 };    // 5 hodnot (indexy 0 - 9)
             int[] a = new int[10];  // indexy 0-9
 
-            //  Načtení hodnot
+            //  Načtení hodnot od uživatele
             //for (int i = 0; i < a.Length; i++)
             //{
             //    a[i] = Convert.ToInt32(Console.ReadLine());
@@ -64,12 +64,12 @@ namespace CSharp_2_Posloupnosti
             }
             Console.WriteLine("Počet sudých je {0}", pocet);
 
-            // Nalezení minimální hodnoty
-            int min = a[0];
-            for (int i = 0; i < a.Length; i++)
-                if (a[i] < min)
-                    min = a[i];
-            Console.WriteLine("Minimum je {0}", min);
+            //// Nalezení minimální hodnoty
+            //int min = a[0];
+            //for (int i = 0; i < a.Length; i++)
+            //    if (a[i] < min)
+            //        min = a[i];
+            //Console.WriteLine("Minimum je {0}", min);
 
             //// Počet minim
             //int pocetMinim = 0;
@@ -83,6 +83,7 @@ namespace CSharp_2_Posloupnosti
             //Console.WriteLine("Minimum = {0} je tam {1} krát.", min, pocetMinim);
 
             // Počet minim v rámci jednoho cyklu
+            int min = a[0];
             int pocetMinim = 1; // Toto výchozí minimum je zam zatím 1x
             for (int i = 1; i < a.Length; i++)  // Od 2. indexu, první už tam je vložený
             {
@@ -108,6 +109,86 @@ namespace CSharp_2_Posloupnosti
                 }
             }
             Console.WriteLine("Minimum = {0} a poprvé je na indexu {1}", a[indexMin], indexMin);
+
+            // Index posledního minima -> <=
+            //int indexMin = 0;
+            indexMin = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] <= a[indexMin])
+                {
+                    indexMin = i;
+                }
+            }
+            Console.WriteLine("Minimum = {0} a naposled je na indexu {1}", a[indexMin], indexMin);
+
+            ////  Odebrání člena posloupnosti
+            //Console.Write("Zadej index člena pro odebrání: ");
+            //int k = Convert.ToInt32(Console.ReadLine());
+            //VypisPosloupnost(a);
+            //for (int i = k; i < a.Length - 1; i++)
+            //{
+            //    a[i] = a[i + 1];
+            //}
+            //Array.Resize(ref a, a.Length - 1);  // Změna velikosti pole - zmenšení
+            //VypisPosloupnost(a);
+
+            ////  Vložení člena posloupnosti
+            //Console.Write("Zadej index člena pro vložení: ");
+            //int k_2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Zadej hodnotu nového člena: ");
+            //int h = Convert.ToInt32(Console.ReadLine());
+            //VypisPosloupnost(a);
+            //Array.Resize(ref a, a.Length + 1);  // Změna velikosti pole - zvětšení
+            //for (int i = a.Length - 1; i > k_2; i--)
+            //{
+            //    a[i] = a[i - 1];
+            //}
+            //a[k_2] = h;
+            //VypisPosloupnost(a);
+
+            // Cyklický posun vpřed (doleva)
+            int p = a[0];   // Do p vložím první člen
+            for (int i = 0; i < a.Length - 1; i++) 
+            {
+                a[i] = a[i + 1];    // Předchozí člen přepíšu následujícím
+            }
+            a[a.Length - 1] = p;    // Poskední člen přepíšu tím prvním
+            VypisPosloupnost(a);
+
+            // Cyklický posun vzad (doprava)
+            p = a[a.Length - 1];   // Do p vložím poslední člen
+            for (int i = a.Length - 1; i > 0; i--)
+            {
+                a[i] = a[i - 1];    // Předchozí člen přepíšu následujícím (ale sestupně, jedu od konce)
+            }
+            a[0] = p;    // První člen přepíšu tím uloženým posledním
+            VypisPosloupnost(a);
+
+            // Práce s více posloupnostmi
+            // Zkopírování sudých členů z A do B
+            int[] b = new int[0];   // Nevím, jak ta posloupnost bude dlouhá
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    Array.Resize(ref b, b.Length + 1);
+                    b[b.Length - 1] += a[i];
+                }
+            }
+            VypisPosloupnost(b);
+
+            //  Spojení posloupností A a B do C
+            int[] c = new int[a.Length + b.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                c[i] = a[i];
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                c[a.Length + i] = b[i];
+            }
+            VypisPosloupnost(c);
 
             Console.ReadKey();
         }
---
+++ Zaklady CSharp/CSharp_3_Matice/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_3_Matice
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            // Deklarace matice
+            int m = 4, n = 5; // 4 řádky, 5 sloupců
+            int[,] a = new int[m, n];
+
+            // Naplnění matice
+
+            // Lze i rovnou
+            int[,] z = new[,]
+            {
+                {1, 2, 3, 4, 5 },
+                {6, 7, 8, 9, 10 },
+                {11, 12, 13, 14, 15 },
+                {16, 17, 18, 19, 20 },
+            };
+
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    a[i, j] = rnd.Next(10);
+            // a[i, j] = i + j * a.GetLength(0) + 1;   // Hodnota vyp. na zíkladě indexů každého člena, na pořadí procházení nazáleží
+            // a[i, j] = j + i * a.GetLength(1) + 1;   // To co výše po řádcích :-)
+
+            //// Jiný způsob naplnění matice náhodnými čísly
+            //for (int i = 0; i < a.Length; i++)
+            //    a[i / a.GetLength(1), i % a.GetLength(1)] = rnd.Next(10);
+
+            VypisMatici(a);
+            //  VypisMatici2(z);
+
+            // Součet hodnot
+            int soucet = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    soucet += a[i, j];
+            Console.WriteLine("Součet členů matice je {0}", soucet);
+            Console.WriteLine("Průměr členů matice je {0}", soucet / (double)a.Length);
+
+            //  Počet sudých členů
+            int pocetSudych = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j] % 2 == 0 && a[i, j] != 0)
+                    {
+                        pocetSudych++;
+                    }
+            Console.WriteLine("Počet sudých členů v matici je {0}", pocetSudych);
+
+            //  Poslední lichá hodnota - po řádcích
+            int poslLich = 2;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j] % 2 == 1)
+                    {
+                        poslLich = a[i, j];
+                    }
+            if (poslLich == 2)
+                Console.WriteLine("V matici žádná lichá hodnota není.");
+            else
+                Console.WriteLine("Poslední lichá hodnota po řádcích je {0}", poslLich);
+
+            //  Poslední lichá hodnota - po sloupcích
+            poslLich = 2;
+            for (int j = 0; j < a.GetLength(1); j++)
+                for (int i = 0; i < a.GetLength(0); i++)
+                    if (a[i, j] % 2 == 1)
+                    {
+                        poslLich = a[i, j];
+                    }
+            if (poslLich == 2)
+                Console.WriteLine("V matici žádná lichá hodnota není.");
+            else
+                Console.WriteLine("Poslední lichá hodnota po sloupcích je {0}", poslLich);
+
+            Console.ReadKey();
+        }
+        static void VypisMatici(int[,] a)
+        {
+            // Vypsání matice
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0,2}, ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void VypisMatici2(int[,] a)
+        {
+            // Jiná verze výpisu matice (1 cyklus, bez čárek na konci řádků)
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("{0,2}{1}", a[i / a.GetLength(1), i % a.GetLength(1)],
+                    (i + 1) % a.GetLength(1) == 0 ? Environment.NewLine : ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
