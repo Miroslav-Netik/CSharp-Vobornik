1e2792a921269d495ea0036564fd0ed49b472d67
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-20 19:41:53 +0100

    Rozprycováno 3. Matice.

--- Zaklady CSharp/CSharp_3_Matice/Program.cs
+++ Zaklady CSharp/CSharp_3_Matice/Program.cs
@@ -83,6 +83,102 @@ namespace CSharp_3_Matice
             else
                 Console.WriteLine("Poslední lichá hodnota po sloupcích je {0}", poslLich);
 
+            // Varianta pro nalezení první liché hodnoty po sloupcich
+            int prvniLich = 2;
+            for (int j = 0; j < a.GetLength(1) && prvniLich == 2; j++)
+            {
+                for (int i = 0; i < a.GetLength(0) && prvniLich == 2; i++)
+                {
+                    if (a[i, j] % 2 != 0)
+                    {
+                        prvniLich = a[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("První lichá hodnota po sloupcích je {0}", prvniLich);
+
+            // Minimální a maximální hodnota
+            int min = a[0, 0];
+            int max = a[0, 1];
+            for (int j = 0; j < a.GetLength(1); j++)
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                    }
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            Console.WriteLine("Nejmenší je {0}\nNejvětší je {1}", min, max);
+
+            // Minimum a počet jeho výskytů
+            min = a[0, 0];
+            int pocetMinim = 0;
+            for (int j = 0; j < a.GetLength(1); j++)
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                        pocetMinim = 1;
+                    }
+                    else if (a[i, j] == min)
+                    {
+                        pocetMinim++;
+                    }
+                }
+            Console.WriteLine("Nejmenších je {0}", pocetMinim);
+
+            // Průměr hodnot na rtém řádku
+            Console.Write("Zadej počet řádků (0 až {0}): ", a.GetLength(0) - 1);
+            int r = int.Parse(Console.ReadLine());
+            int soucetR = 0;
+            for (int j = 0; j < a.GetLength(1); j++)
+                soucetR += a[r, j];
+            Console.WriteLine("Průměr hodnot matice na řádku s indexem {0} je {1}", r,
+                soucetR / (double)a.GetLength(1));
+
+            //  Záměna prvního a posledního řádku
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                int p = a[0, j];
+                a[0,j] = a[a.GetLength(0) - 1, j];
+                a[a.GetLength(0) - 1, j] = p;
+            }
+            Console.WriteLine("\nMatice se zaměněnými řádky:");
+            VypisMatici(a);
+
+            //// Součin prvků z s-tého sloupce
+            //Console.Write("Zadej index sloupce (0 až {0}): ", a.GetLength(1) - 1);
+            //int s = int.Parse(Console.ReadLine());
+            //int soucinS = 1;
+            //for (int i = 0; i < a.GetLength(0); i++)
+            //{
+            //    soucinS *= a[i, s];
+            //}
+            //Console.WriteLine("Souči hodnot sloupce s indexem {0} je {1}", s, soucinS);
+
+            //  Průměry ze všech sloupců
+            double[] prS = new double[a.GetLength(1)];
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                int soucetS = 1;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    soucetS += a[i, j];
+                }
+                prS[j] = soucetS / (double)a.GetLength(0);
+            }
+            Console.Write("Průměry sloupců: ");
+            for (int i = 0; i < prS.Length; i++)
+            {
+                Console.Write("{0}; ", prS[i]);
+            }
+            Console.WriteLine();
+
             Console.ReadKey();
         }
         static void VypisMatici(int[,] a)
